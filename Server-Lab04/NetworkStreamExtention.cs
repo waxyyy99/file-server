05fa520a95b80d8ec6a875a4a6097afd11d01530
waxyyy99 <waxyyy99@bk.ru>
2025-04-20 14:31:14 +0300

    feat: implemented NetworkStream extention for files

--- Server-Lab04/NetworkStreamExtention.cs
+++ Server-Lab04/NetworkStreamExtention.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,11 +13,11 @@ namespace Server
         public static async Task<string> ReadStringAsync(this NetworkStream ns)
         {
             byte[] conentLengthBuffer = new byte[4];
-            int bytesRead = await ns.ReadAsync(conentLengthBuffer, 0, 4);
+            await ns.ReadExactlyAsync(conentLengthBuffer);
             int contentLength = BitConverter.ToInt32(conentLengthBuffer, 0);
 
             byte[] buffer = new byte[contentLength];
-            bytesRead = await ns.ReadAsync(buffer, 0, buffer.Length);
+            int bytesRead = await ns.ReadAsync(buffer, 0, buffer.Length);
 
             return Encoding.UTF8.GetString(buffer, 0, bytesRead);
         }
@@ -25,9 +26,39 @@ namespace Server
         {
             byte[] bytes = Encoding.UTF8.GetBytes(message);
             var contentLengthByte = BitConverter.GetBytes(bytes.Length);
-
             await ns.WriteAsync(contentLengthByte, 0, contentLengthByte.Length);
             await ns.WriteAsync(bytes, 0, bytes.Length);
+        }
+
+        public static async Task WriteFileAsync(this NetworkStream ns, string path, CancellationToken cancellationToken)
+        {
+            const int bufferSize = 81920;
+            var fileInfo = new FileInfo(path);
+
+            await ns.WriteAsync(BitConverter.GetBytes(fileInfo.Length), 0, 8, cancellationToken);
+
+            using var file = File.OpenRead(path);
+            await file.CopyToAsync(ns, bufferSize, cancellationToken);
+        }
+        public static async Task ReadFileAsync(this NetworkStream ns, string path, CancellationToken cancellationToken)
+        {
+            byte[] length = new byte[8];
+            await ns.ReadExactlyAsync(length, cancellationToken);
+            long fileLength = BitConverter.ToInt64(length, 0);
+
+            const int bufferSize = 81920;
+            byte[] buffer = new byte[bufferSize];
+            long recieved = 0;
+
+            using var fs = File.Create(path);
+            while (recieved < fileLength)
+            {
+                int toRead = (int)Math.Min(fileLength - recieved, bufferSize);
+                int read = await fs.ReadAsync(buffer, 0, toRead, cancellationToken);
+                if (read == 0) throw new IOException("Сокет преждевременно закрыл соединение.");
+                await fs.WriteAsync(buffer, 0, read, cancellationToken);
+                recieved += read;
+            }
         }
     }
 }
